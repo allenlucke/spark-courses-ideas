88a59ee95b742b7e17c14d409378254bc38fbcc3
Allen Lucke <allenlucke@gmail.com>
2020-05-01 13:43:35 -0500

    initial commit

---
+++ src/main/java/com/allen/courses/Main.java
@@ -0,0 +1,12 @@
+package com.allen.courses;
+
+import static spark.Spark.get;
+
+public class Main {
+
+    public static void main(String[] args ) {
+        get("/hello", (req, res) -> "Hello World");
+
+        get("/", (req, res) -> "Welcome Students");
+    }
+}
